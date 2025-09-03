a3d8fa6e5d09d2f2873993e9806e2469faf5d0d5
Shunta Ohashi <185430250+Shunta-Ohashi@users.noreply.github.com>
2025-09-03 14:43:38 +0900

    ・電卓のレイアウト修正 ・画面遷移の演習 →明示的Intent（MainActivity2からMainActivity） →暗黙的Intent（MainActivity2から地図（座標：名城大学））

--- app/src/main/java/jp/ac/meijou/android/s241205034/MainActivity2.java
+++ app/src/main/java/jp/ac/meijou/android/s241205034/MainActivity2.java
@@ -1,5 +1,7 @@
 package jp.ac.meijou.android.s241205034;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,17 +10,36 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import jp.ac.meijou.android.s241205034.databinding.ActivityMain2Binding;
+
 public class MainActivity2 extends AppCompatActivity {
+
+    private ActivityMain2Binding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main2);
+        binding = ActivityMain2Binding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        // 明示的Intent
+        binding.button2.setOnClickListener(view -> {
+            var intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+        });
+
+        // 暗黙的Intent
+        binding.button3.setOnClickListener(view -> {
+            var intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:35.13537864, 136.97517254"));
+            startActivity(intent);
         });
     }
 }
